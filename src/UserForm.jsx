d3d8c42368ca6a528a105847f2989d1404f219f5
kanyaweeN <40629441+kanyaweeN@users.noreply.github.com>
2025-09-15 15:03:59 +0700

    fix:fix bug

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -5,20 +5,26 @@ export default function UsernameForm() {
 
   function handleSubmit(e) {
     e.preventDefault();
+
     alert(`Submitted: ${username}`);
+    setUsername("");
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username
-        <input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Enter username"
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter username"
+          />
+        </label>
+        <button type="submit" disabled={username.trim().length < 3}>Submit</button>
+      </form>
+      {(username.trim().length < 3) && <span className="error">กรุณาใส่ username อยากน้อย 3 ตัวอักษร</span>}
+    </div>
+
   );
 }
